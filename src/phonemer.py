317a840200c43a5011dd2e812fc5796ebd8ef343
Alexander Dean <dstar@csh.rit.edu>
2012-04-25 10:00:43 -0400

    Added phonemer exec file.

---
+++ src/phonemer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# Usage:
+#   ./phonemer.py [--train=<TRAINING_FILE>] <WORD>
+# 
+
+import sys
+import nltk
+
+__usage__ = "%s [--train=<TRAINING_FILE>] <WORD>"
+
+
+
+if __name__ == "__main__":
+    print __usage__%sys.argv[0]
